b799f378fe0596912b3536bc840e9a9ba6867b54
Dhuha Hasan <duha_9@yahoo.com>
2019-11-29 22:23:07 +0000

    Done.

--- main.js
+++ main.js
@@ -1,12 +1,35 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
-
 // Your JavaScript code goes here!
 
-
-
-
+function handleHeartClick() {
+  let errorDiv = document.getElementById("modal")
+  let ul = document.getElementsByTagName("ul")
+  for (let i = 0; i < ul.length; i++) {
+    let span = ul[i].getElementsByTagName("li")[0].getElementsByTagName("span")[0]
+    let li = ul[i].getElementsByTagName("li")[0]
+    li.onclick = function () {
+      if (span.innerHTML === EMPTY_HEART) {
+        mimicServerCall().then(res => {
+          span.innerHTML = FULL_HEART
+          span.className = "activated-heart"
+        }).catch(err => {
+          errorDiv.classList.remove("hidden")
+          let h2 = errorDiv.getElementsByTagName("h2")[0]
+          h2.innerText = err
+          setTimeout(() => {
+            errorDiv.className = "hidden"
+          }, 5000);
+        })
+      } else {
+        span.classList.remove("activated-heart")
+        span.innerHTML = EMPTY_HEART
+      }
+    }
+  }
+}
+handleHeartClick()
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
 //------------------------------------------------------------------------------
