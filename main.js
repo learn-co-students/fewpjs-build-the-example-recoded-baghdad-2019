5d1ec8537c9f522c336f3a0e15c9546c878d60eb
Wolverine964 <W964@users.noreply.github.com>
2019-12-17 14:59:39 +0300

    Done

--- main.js
+++ main.js
@@ -1,12 +1,25 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
-
-// Your JavaScript code goes here!
-
-
-
-
+let colorStates = {
+  "red" : "",
+  "": "red"
+};
+let articleHearts = document.querySelectorAll(".like");
+function likeCallback(e) {
+  let heart = e.target;
+  mimicServerCall("bogusUrl")
+    .then(function(serverMessage){
+       heart.innerText = glyphStates[heart.innerText];
+       heart.style.color = colorStates[heart.style.color];
+    })
+    .catch(function(error) {
+      document.getElementById("modal").className = "hidden";
+    });
+}
+for (let glyph of articleHearts) {
+  glyph.addEventListener("click", likeCallback);
+}
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
 //------------------------------------------------------------------------------
